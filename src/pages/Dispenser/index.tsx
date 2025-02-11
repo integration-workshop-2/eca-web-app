ac4d89c8cd2a3e06d3a1f0c2a84c6b4072ebd78c
RuKuznierS <rudineikuzniersilva@gmail.com>
2025-02-11 18:38:26 -0300

    feat: Implementation of delete and update in medicine

--- src/pages/Dispenser/index.tsx
+++ src/pages/Dispenser/index.tsx
@@ -7,6 +7,7 @@ import MedicineService from "../../services/medicineService";
 import PopupUpdate from "../../components/layout/Miscellaneus/PopupUpdate";
 import PopupDelete from "../../components/layout/Miscellaneus/PopupDelete";
 import routineService from "../../services/routineService";
+import medicineService from "../../services/medicineService";
 interface Medicine {
     id: string;
     name: string;
@@ -41,56 +42,69 @@ const Dispenser: React.FC = () => {
     const [dataMedicine, setDataMedicine] = useState<Medicine[]>([]);
     const [dataRoutine, setDataRoutine] = useState<Routine[]>([]);
     useEffect(() => {
-        async function fetchMedicine() {
-            try {
-                const medicineData = await MedicineService.all();
-        
-                setDataMedicine(medicineData);
-            } catch (error) {
-                console.error("Error fetching medicines:", error); 
-            }
-        }
-
-        async function fetchDataRoutine() {
-            try {
-                const routineData: Routine[] = await routineService.all(); 
-                setDataRoutine(routineData);
-            } catch (error) {
-                console.error("Error fetching routines:", error);
-            }
-        }
-
         fetchMedicine();
         fetchDataRoutine();
     }, []);
 
+    const fetchMedicine = async () => {
+        try {
+            const medicineData = await MedicineService.all();
+
+            setDataMedicine(medicineData);
+        } catch (error) {
+            console.error("Error fetching medicines:", error);
+        }
+    }
+
+    const fetchDataRoutine = async () => {
+        try {
+            const routineData: Routine[] = await routineService.all();
+            setDataRoutine(routineData);
+        } catch (error) {
+            console.error("Error fetching routines:", error);
+        }
+    }
 
     const [selectedMedicine, setSelectedMedicine] = useState<Medicine | null>(null);
     const [selectedRoutine, setSelectedRoutine] = useState<Routine | null>(null);
 
-    const handleUpdateMedicine = (row: Record<string, any>) => {
-        const medicine = row as Medicine;
-        setSelectedMedicine(medicine);
+    const handleUpdateMedicine = async (medicine: Medicine) => {
         setIsUpdating(true);
+        try {
+            await medicineService.updateMedicine(medicine.id, medicine.name, medicine.cylinder_number);
+        } catch (error) {
+            console.error('Erro ao atualizar o nome:', error);
+        } finally {
+            setIsUpdating(false);
+            fetchMedicine();
+            alert("Dados do paciente atualizados com sucesso!");
+        }
     };
 
-    const handleDeleteMedicine = (row: Record<string, any>) => {
-        const medicine = row as Medicine;
-        setSelectedMedicine(medicine);
-        setIsDeleting(true);
 
+    const handleDeleteMedicine = async (medicine: Medicine) => {
+        setIsDeleting(true);
+        try {
+            await medicineService.deleteMedicine(medicine.id);
+        } catch (error) {
+            console.error('Erro ao deletar o usuário:', error);
+        } finally {
+            setIsDeleting(false);
+            fetchMedicine();
+            alert("O medicamento foi excluído!")
+        }
     };
 
     const handleUpdatePatient = (row: Record<string, any>) => {
         const user = row as Routine;
-        setSelectedRoutine(user);
-        setIsUpdating(true);
+        //setSelectedRoutine(user);
+        //setIsUpdating(true);
     };
 
     const handleDeletePatient = (row: Record<string, any>) => {
         const user = row as Routine;
-        setSelectedRoutine(user);
-        setIsDeleting(true);
+        // setSelectedRoutine(user);
+        //setIsDeleting(true);
 
     };
 
@@ -109,8 +123,16 @@ const Dispenser: React.FC = () => {
                 <Table
                     columns={columnsMedicine}
                     data={dataMedicine}
-                    onUpdate={handleUpdateMedicine}
-                    onDelete={handleDeleteMedicine}
+                    onUpdate={(row) => {
+                        const medicine = row as Medicine;
+                        setSelectedMedicine(medicine);
+                        setIsUpdating(true);
+                    }}
+
+                    onDelete={() => {
+                        if(selectedMedicine)
+                        handleDeleteMedicine(selectedMedicine)
+                    }}
                 />
             </div>
 
@@ -122,20 +144,28 @@ const Dispenser: React.FC = () => {
                     onDelete={handleDeletePatient}
                 />
             </div>
-            {isUpdating && (
-            <PopupUpdate
-                item={{ nome: "Carlos", status: "Ativo" }}
-                title="Editar medicamento"
-                image="logo192.png"
-                fields={[
-                    { key: "nome", label: "Nome", type: "text" },
-                    { key: "status", label: "Cilindro", type: "select", options: ["1", "2", "3"] }
-                ]}
-                onClose={() => setIsUpdating(false)}
-                onUpdate={(updatedItem) => console.log("Atualizado:", updatedItem)}
-            />
+            {isUpdating && selectedMedicine && (
+                <PopupUpdate
+                    item={selectedMedicine as Medicine}
+                    title="Editar medicamento"
+                    image="logo192.png"
+                    fields={[
+                        { key: "name", label: "Nome", type: "text" },
+                        { key: "cylinder_number", label: "Cilindro", type: "select", options: ["1", "2", "3"] }
+                    ]}
+                    onClose={() => setIsUpdating(false)}
+                    onUpdate={(updatedItem) => {
+                        if (selectedMedicine)
+                            handleUpdateMedicine({
+                                id: selectedMedicine.id,
+                                name: updatedItem.name,
+                                cylinder_number: updatedItem.cylinder_number,
+                            });
+                    }}
+
+                />
             )}
-           
+
 
         </>
     );
